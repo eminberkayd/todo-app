f2496db21848393a959c3f10ffdc63f7fe277fa3
emin-berkay-daglar <eminberkaydaglar@gmail.com>
2022-08-11 14:58:28 +0300

    last commit- project completed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
-import Header from "./components/Header";
-import { useState } from 'react';
-import Todo from "./components/Todo";
+
+import Container from "./components/Container";
 
 function App() {
-  const [todos, settodos] = useState([]);
-  const todolist = [];
-  for (let i = 0; i < todos.length; i++) {
-    todolist.push(<Todo key={i} index={i} todo={todos[i]} editordel={settodos} />)
-  }
-  return (
-    <div className="container">
-      <Header addnew={settodos} />
-      <table className="table table-hover">
-        <thead></thead>
-        <tbody>
-          {todolist}
-        </tbody>
-      </table>
 
-    </div>
+  return (
+    <>
+      <Container />
+    </>
   );
 }
 
